3c4a1e1b2854e78f68881497a4da3b9f4ef03b70
Andrew Young <youngar17@gmail.com>
2013-02-22 01:53:13 -0500

    Add initial door sensor

---
+++ src/cpp/doorstatesensor.h
@@ -0,0 +1,18 @@
+#ifndef _DOORSTATESENSOR_H_
+#define _DOORSTATESENSOR_H_
+
+/*  TODO: implement door sensor */
+namespace roomsec {
+  enum DoorState {open, closed};
+
+  class DoorStateSensor {
+    public:
+
+    private:
+      DISALLOW_COPY_AND_ASSIGN(DoorStateSensor);
+
+  };
+}
+
+#endif /* _DOORSTATESENSOR */
+
