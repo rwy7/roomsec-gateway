e16022615b3f1ca73b3c6bee6085694ce08cfea6
Robert Young <rwy0717@gmail.com>
2013-03-12 22:37:45 -0400

    Document thrift authority adapter

--- src/cpp/thriftauthorityadapter.h
+++ src/cpp/thriftauthorityadapter.h
@@ -29,6 +29,14 @@ namespace apache { namespace thrift {
 
 namespace roomsec {
 
+  /**
+   * An adapter to a remotely placed authority process.  Communication
+   * between the adapter and the authority occurs over an ip-based
+   * thrift protocol.
+   *
+   * The exact thrift protocol is unspecified, leaving the decision up
+   * to the policy of higher level modules.
+   */
   class ThriftAuthorityAdapter: public AuthorityAdapter {
   public:
     ThriftAuthorityAdapter(const std::string& authorityServer,
@@ -44,9 +52,20 @@ namespace roomsec {
 
   private:
 
+    /**
+     * Obtain a record of required credentials for a resource.
+     *
+     * This resource is generated and produced by a remote authority agent.
+     *
+     * @param[out] requirements  A specification of required credentials.
+     * @param[in]  resource      a URL which uniquely identifies the resource.
+     */
     void checkRequirementsImpl(std::vector<iface::CredentialSpec>& requirements,
 			       const std::string& resource);
 
+    /**
+     * Defer an authorization decision to a remotely placed authority process.
+     */
     iface::AuthorizationReply::type authorizeImpl(const iface::AuthorizationRequest& request);
   };
 }
