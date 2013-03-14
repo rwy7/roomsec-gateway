1f7c7e29fad169f67afd09cb24936157623de359
Andrew Young <youngar17@gmail.com>
2013-03-14 19:05:33 -0400

    Add mainpage documentation

---
+++ src/cpp/main.h
@@ -0,0 +1,17 @@
+/*  Main Header File */
+
+#ifndef _MAIN_H_
+#define _MAIN_H_
+
+ /**
+ * @mainpage RoomSec Gateway
+ *
+ * @section  intro_sec Introduction
+ *
+ * 
+ * This is the RoomSec Gateway.  It represents the software that runs at each
+ * gateway.
+ */
+
+#endif /* _MAIN_H_ */
+
