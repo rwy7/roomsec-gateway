5c0c89e6f149fd3c31080765893d375c3bed9038
Andrew Young <youngar17@gmail.com>
2013-03-14 21:00:35 -0400

    Create generic door state sensor.

--- src/cpp/doorstatesensor.h
+++ src/cpp/doorstatesensor.h
@@ -1,14 +1,31 @@
 #ifndef _DOORSTATESENSOR_H_
 #define _DOORSTATESENSOR_H_
 
-/*  TODO: implement door sensor */
 namespace roomsec {
+
+  /**
+   * @brief Whether the door is open or closed.
+   */
   enum DoorState {open, closed};
 
+
+  /**
+   * @brief Senses if a door is opened or closed.
+   */
   class DoorStateSensor {
+
     public:
-      DoorStateSensor(int pin);
-      getDoorState()
+
+      /**
+       * @brief Get the current door state.
+       *
+       * This will poll the sensor, ensuring up-to-date information.  No
+       * results are memoized.
+       *
+       * @return The current door state.
+       */
+      virtual DoorState getDoorState();
+
     private:
       DISALLOW_COPY_AND_ASSIGN(DoorStateSensor);
 
