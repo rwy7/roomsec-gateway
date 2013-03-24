098fd3f3c642757bc387af86f49912a5b3a47b10
Andrew Young <youngar17@gmail.com>
2013-03-24 02:44:02 -0400

    Implement GPIO expander

---
+++ src/cpp/ioexpander.cpp
@@ -0,0 +1,148 @@
+#include <iostream>
+#include <cassert>
+#include "wiringPi/wiringPi.h"
+#include "wiringPi/wiringPiI2C.h"
+#include "ioexpander.h"
+
+namespace roomsec {
+  /* MCP23017 Registers
+   * See the manual for more information about what these mean 
+   */
+  enum {
+    IOCON          = 0x0A,
+
+    IODIRA         = 0x00,
+    IPOLA          = 0x02,
+    GPINTENA       = 0x04,
+    DEFVALA        = 0x06,
+    INTCONA        = 0x08,
+    GPPUA          = 0x0C,
+    INTFA          = 0x0E,
+    INTCAPA        = 0x10,
+    //GPIOA        = 0x12, (in the header file)
+    OLATA          = 0x14,
+
+    IODIRB        =  0x01,
+    IPOLB         =  0x03,
+    GPINTENB      =  0x05,
+    DEFVALB       =  0x07,
+    INTCONB       =  0x09,
+    GPPUB         =  0x0D,
+    INTFB         =  0x0F,
+    INTCAPB       =  0x11,
+    //GPIOB       =  0x13, (in the header file)
+    OLATB         =  0x15,
+
+    // Bits in the IOCON register
+    IOCON_BANK_MODE = 0x80,
+    IOCON_MIRROR    = 0x40,
+    IOCON_SEQOP     = 0x20,
+    IOCON_DISSLW    = 0x10,
+    IOCON_HAEN      = 0x08,
+    IOCON_ODR       = 0x04,
+    IOCON_INTPOL    = 0x02,
+    IOCON_UNUSED    = 0x01,
+
+    /*  Default Settings */
+    IOCON_INIT      = (IOCON_SEQOP)
+  };
+
+  IOExpander::IOExpander () : gpioa(0), gpiob(0), iodira(0), iodirb(0) {
+  }
+
+  IOExpander::~IOExpander () {
+
+  }
+
+  void IOExpander::initialize (int dev) {
+    if ((dev = wiringPiI2CSetup (dev)) == -1) {
+      std::cout << "Unable to create device";
+      assert (dev > 0);
+      //TODO: raise an exception
+      //TODO: Log something
+    }
+
+    /*  Initialize the device */
+    wiringPiI2CWriteReg8 (dev, IOCON, IOCON_INIT);
+    wiringPiI2CWriteReg8 (dev, IODIRA, 0x00);
+    wiringPiI2CWriteReg8 (dev, IODIRB, 0x00);
+    wiringPiI2CWriteReg8 (dev, GPIOA, 0x00);
+    wiringPiI2CWriteReg8 (dev, GPIOB, 0x00);
+
+    this->gpioa = 0x00;
+    this->gpiob = 0x00;
+
+    return;
+  }
+
+  void IOExpander::setRW (GPIO bank, uint8_t pins) {
+    assert (bank == GPIOA || bank == GPIOB);
+    if (bank == GPIOA) {
+      wiringPiI2CWriteReg8 (dev, IODIRA, pins);
+      iodira = pins;
+    }
+    else if (bank == GPIOB) {
+      wiringPiI2CWriteReg8 (dev, IODIRB, pins);
+      iodirb = pins;
+    }
+  }
+
+  uint8_t IOExpander::getRW (GPIO bank) {
+    assert (bank == GPIOA || bank == GPIOB);
+    uint8_t rw = 0;
+    if (bank == GPIOA) {
+      rw = iodira;
+    }
+    else if (bank == GPIOB) {
+      rw = iodirb;
+    }
+    return rw;
+  }
+
+  void IOExpander::makeHigh (GPIO bank, uint8_t pins) {
+    assert (bank == GPIOA || bank == GPIOB);
+    uint8_t *gpio = NULL;
+    if (bank == GPIOA) {
+      gpio = &this->gpioa;
+    }
+    else if (bank == GPIOB) {
+      gpio = &this->gpiob;
+    }
+    else
+      assert (1);
+
+    *gpio |= pins;
+    wiringPiI2CWriteReg8 (this->dev, bank, *gpio);
+    return;
+  }
+
+  void IOExpander::makeLow (GPIO bank, uint8_t pins) {
+    assert (bank == GPIOA || bank == GPIOB);
+    uint8_t *gpio = NULL;
+    if (bank == GPIOA) {
+      gpio = &this->gpioa;
+    }
+    else if (bank == GPIOB) {
+      gpio = &this->gpiob;
+    }
+    else
+      assert (1);
+
+    *gpio &=!pins;
+    wiringPiI2CWriteReg8 (this->dev, bank, *gpio);
+    return;
+  }
+
+  void IOExpander::send (GPIO bank, uint8_t value) {
+    assert (bank == GPIOA || bank == GPIOB);
+    wiringPiI2CWriteReg8 (this->dev, bank, value);
+    return;
+  }
+
+  uint8_t IOExpander::read(GPIO bank) {
+    assert (bank == GPIOA || bank == GPIOB);
+    return wiringPiI2CReadReg8 (dev, bank);
+  }
+
+} /*  namespace roomsec */
+
--- src/cpp/ioexpander.h
+++ src/cpp/ioexpander.h
@@ -22,82 +22,86 @@ namespace roomsec {
 
       /**
        * @brief Create a device handler
+       */
+      IOExpander ();
+
+      ~IOExpander ();
+
+      /**
+       * @brief The GPIO bank of the device.
+       * There are two banks on each device, GPIOA and GPIOB. On each bank there
+       * are 8 pins.  Each pin can be read to or written to.
+       */
+      enum GPIO {GPIOA = 0x12, GPIOB = 0x13};
+
+      /**
+       * @brief Initial the device.
+       * This must be called before the device can be used.  It will set all pins
+       * to write 0.
        *
        * @param dev The I2C device number.
        */
-    IOExpander (int dev);
+      void initialize(int dev);
 
-    ~IOExpander ();
+      /**
+       * @brief Set each pin to READ or WRITE mode.
+       * If a pin is 1, it is set to read mode.
+       * If a pin is 0, it is set to write mode.
+       *
+       * @param bank The GPIO bank.
+       * @param pins The pin R/W setting.
+       */
+      void setRW (GPIO bank, uint8_t pins);
 
-    /**
-     * @brief The GPIO bank of the device.
-     * There are two banks on each device, GPIOA and GPIOB. On each bank there
-     * are 8 pins.  Each pin can be read to or written to.
-     */
-    enum GPIO {A = 0x12, B = 0x13};
+      /**
+       * @brief Get whether pins are READ or WRITE mode.
+       * See setIO for more information.
+       *
+       * @param bank The GPIO bank.
+       *
+       * @return The pin R/W setting.
+       */
+      uint8_t getRW (GPIO bank);
 
-    /**
-     * @brief Initial the device.
-     * This must be called before the device can be used.  It will set all pins
-     * to read mode.
-     */
-    void initialize();
+      /**
+       * @brief Make the specified pins HIGH.
+       * This will leave unspecified pins at the same value.
+       *
+       * @param bank The GPIO bank.
+       * @param pins The pins to be set to HIGH.
+       */
+      void makeHigh (GPIO bank, uint8_t pins);
 
-    /**
-     * @brief Set each pin to READ or WRITE mode.
-     * If a pin is 0, it is set to read mode.
-     * If a pin is 1, it is set to write mode.
-     *
-     * @param bank The GPIO bank.
-     * @param pins The pin R/W setting.
-     */
-    void setRW (GPIO bank, uint8_t pins);
+      /**
+       * @brief Make the specified pins LOW.
+       * This will leave unspecified pins at the same value.
+       *
+       * @param bank The GPIO bank.
+       * @param pins The pins to be set to LOW.
+       */
+      void makeLow (GPIO bank, uint8_t pins);
 
-    /**
-     * @brief Get whether pins are READ or WRITE mode.
-     * See setIO for more iformation.
-     *
-     * @param bank The GPIO bank.
-     *
-     * @return The pin R/W setting.
-     */
-    uint8_t getRW (GPIO bank);
+      /**
+       * @brief Write to all pins on a GPIO bank
+       * Values written to read only pins will be ignored.
+       *
+       * @param bank The GPIO bank
+       * @param value The value for each pin.
+       */
+      void send (GPIO bank, uint8_t value);
 
-    /**
-     * @brief Make the specified pins HIGH.
-     * This will leave unspecified pins at the same value.
-     *
-     * @param bank The GPIO bank.
-     * @param pins The pins to be set to HIGH.
-     */
-    void makeHigh (GPIO bank, uint8_t pins);
+      /**
+       * @brief Read all pins from a GPIO bank
+       * This will give the value of all pins.
+       * @param bank The GPIO bank.
+       *
+       * @return The value of each pin.
+       */
+      uint8_t read(GPIO bank);
 
-    /**
-     * @brief Make the specified pins LOW.
-     * This will leave unspecified pins at the same value.
-     *
-     * @param bank The GPIO bank.
-     * @param pins The pins to be set to LOW.
-     */
-    void makeLow (GPIO bank, uint8_t pins);
-    
-    /**
-     * @brief Write to all pins on a GPIO bank
-     * Values written to read only pins will be ignored.
-     *
-     * @param bank The GPIO bank
-     * @param value The value for each pin.
-     */
-    void send (GPIO bank, uint8_t value);
-
-    /**
-     * @brief Read all pins from a GPIO bank
-     * This will give the value of all pins.
-     * @param bank The GPIO bank.
-     *
-     * @return The value of each pin.
-     */
-    uint8_t read(GPIO bank);
+    protected:
+      int dev;
+      uint8_t gpioa, gpiob, iodira, iodirb;
   };
 }
 
