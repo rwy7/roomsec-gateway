19083783faa8a1ae0c6a6deb3caa1f0881b6f007
Tyler Atkin <atkintc@gmail.com>
2013-03-24 08:15:21 -0400

    add virtual destructor to blocksensor

--- src/cpp/blocksensor.h
+++ src/cpp/blocksensor.h
@@ -9,18 +9,20 @@ namespace roomsec{
 	 */
 class BlockSensor{
 	protected:
-	static const int iLow = 0, iHigh = 100;
-	float mSensorValue;
-	int iSensorValue;
+	//static const int iLow = 0, iHigh = 100;
+	//float mSensorValue;
+	//int iSensorValue;
+
 	public:
-	
+	virtual ~BlockSensor(){}
+
 	/**
 	 * @brief returns the voltage signal from the sensor mapped to an integer range from iLow..iHigh
 	 *
 	 * iLow corresponds to the signal for the maximum detection range on the sensor.
 	 * iHigh corresponds to the signal for the minimum detection range on the sensor.
 	 */
-	virtual int getSensorValue();
+	virtual int getSensorValue()=0;
 };
 
 }	
