b624da90802675da2678ea5d19357120a9f91c6c
Andrew Young <youngar17@gmail.com>
2013-03-24 08:25:24 -0400

    Implement display virtual funcs in lcddisplay
    (linking error*

--- src/cpp/lcddisplay.cpp
+++ src/cpp/lcddisplay.cpp
@@ -114,5 +114,14 @@ namespace roomsec {
     putCommand (LCD_RS & 'A');
     putCommand (LCD_RS & 'b');
   }
+
+  void LCDDisplay::setColor(Color c){}
+  void LCDDisplay::setDisplay(int row, int col){}
+  void LCDDisplay::putChar(char character){}
+  void LCDDisplay::clear(){}
+  void LCDDisplay::home(){}
+  std::pair<int, int> LCDDisplay::size(){ return std::make_pair(0, 0);}
+  int LCDDisplay::rows(){}
+  int LCDDisplay::cols(){}
 }
 
