8895d372240d66c39b6d32b672f9cbe0e0b9c6b7
Robert Young <rwy0717@gmail.com>
2013-03-25 19:00:28 -0400

    Remove ioexpander include, fix indentation for LcdDisplay

--- src/cpp/lcddisplay.h
+++ src/cpp/lcddisplay.h
@@ -1,73 +1,78 @@
+/* -*- Mode: c++ -*- */
 #ifndef _LCDDISPLAY_H_
 #define _LCDDISPLAY_H_
 
 #include "boost/shared_ptr.hpp"
-#include "ioexpander.h"
+//#include "ioexpander.h"
 #include "display.h"
 
 namespace roomsec {
+
+  class IOExpander;
+
   /**
    * @brief An LCD Screen attached to a I2C IOExpander.
    * The LCD requires a specific setup to work - there is not many options
    * here.
    */
   class LCDDisplay : public Display {
-    public:
-      LCDDisplay(boost::shared_ptr<IOExpander> expander);
-      ~LCDDisplay(); 
-      void initialize();
+  public:
+    LCDDisplay(boost::shared_ptr<IOExpander> expander, uint bank);
+    ~LCDDisplay();
+    void initialize();
 
-      /**
-       * Set the backlight color of the display.  This call should
-       * update the screen immediately.
-       */
-      void setColor(Color c);
+    /**
+     * Set the backlight color of the display.  This call should
+     * update the screen immediately.
+     */
+    void setColor(Color c);
 
-      /**
-       * @brief set the cursor to [row, col].
-       */
-      void setDisplay(int row, int col);
+    /**
+     * @brief set the cursor to [row, col].
+     */
+    void setDisplay(int row, int col);
 
-      /**
-       * @brief Put the character onto the lcd at the current position
-       *
-       * @param character
-       */
-      void putChar(char character);
+    /**
+     * @brief Put the character onto the lcd at the current position
+     *
+     * @param character
+     */
+    void putChar(char character);
 
-      void putStr(std::string stuff); 
-      /**
-       * @brief Clear the lcd screen.
-       */
-      void clear();
+    void putStr(std::string stuff);
 
-      /**
-       * @brief Move the cursor to [0,0]
-       */
-      void home();
+    /**
+     * @brief Clear the lcd screen.
+     */
+    void clear();
 
-      /**
-       * Returns the size (rows, cols)
-       */
-      std::pair<int, int> size();
+    /**
+     * @brief Move the cursor to [0,0]
+     */
+    void home();
 
-      /**
-       * Returns the height of the display in character rows.
-       */
-      int rows();
+    /**
+     * Returns the size (rows, cols)
+     */
+    std::pair<int, int> size();
 
-      /**
-       * Returns the width of screen in character columns.
-       */
-     int cols();
+    /**
+     * Returns the height of the display in character rows.
+     */
+    int rows();
 
-    protected:
-      void strobe();
-      void sendDataCmd(uint8_t data);
-      void putCommand(uint8_t data);
-      void put4Command(uint8_t data);
+    /**
+     * Returns the width of screen in character columns.
+     */
+    int cols();
 
-      boost::shared_ptr<IOExpander> expander;
+  protected:
+    void strobe();
+    void sendDataCmd(uint8_t data);
+    void putCommand(uint8_t data);
+    void put4Command(uint8_t data);
+
+    boost::shared_ptr<IOExpander> expander;
   };
 }
 
