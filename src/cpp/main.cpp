e623bbc4e1ffbb2353a20d3298b91cbfb2166baa
Robert Young <rwy0717@gmail.com>
2013-03-27 13:52:54 -0400

    Insert testing code for random failure into main

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -55,25 +55,30 @@ int main (int argc, char *argv[]) {
     /* Initialize the screen hardware system
      */
     LOG4CXX_DEBUG(logger, "Starting LCD");
-
+    LOG4CXX_DEBUG(logger, "IOExpander");
     boost::shared_ptr<roomsec::IOExpander> expander (new roomsec::IOExpander());
     expander->initialize(0x20);
 
+    LOG4CXX_DEBUG(logger, "LCDDisplay");
     boost::shared_ptr<roomsec::LCDDisplay> disp(new roomsec::LCDDisplay(expander));
     disp->initialize();
 
-    boost::shared_ptr<roomsec::Buzzer> buzzer(new roomsec::Buzzer(17));
+    // boost::shared_ptr<roomsec::Buzzer> buzzer(new roomsec::Buzzer(17));
+    // boost::shared_ptr<roomsec::Ui> ui(new roomsec::Ui(disp, buzzer));
 
-    boost::shared_ptr<roomsec::Ui> ui(new roomsec::Ui(disp, buzzer));
-
+    LOG4CXX_DEBUG(logger, "Printing to Display");
     disp->putStr("    RoomSec");
     disp->setDisplay(1, 0);
     disp->putStr("initializing...");
+
+    LOG4CXX_DEBUG(logger, "Setting backlight pins");
     disp->setBacklightPins(expander->GPIOB, 0x01, 0x02, 0x04);
-    disp->setBacklightColor(disp->blue);
 
-    ui->message(roomsec::UiMessage::Type::error, "Hello, world!");
+    LOG4CXX_DEBUG(logger, "Setting backlight color to RED");
+    disp->setBacklightColor(disp->red);
 
+    LOG4CXX_DEBUG(logger, "Building Gateway");
+    //ui->message(roomsec::UiMessage::Type::error, "Hello, world!");
     buildReplGateway(vm)->start();
   }
 
@@ -120,6 +125,8 @@ int initLogging(po::variables_map& vm) {
 
 int initHardware(po::variables_map& vm) {
   int retVal = 0;
+
+  LOG4CXX_DEBUG(logger, "Initializing Hardware");
 
 #ifdef ENABLE_GATEWAY
   if (wiringPiSetup() == -1) {
