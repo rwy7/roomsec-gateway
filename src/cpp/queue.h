058485ee6cddd47bfd7995fd9f0314806ff19b93
Robert Young <rwy0717@gmail.com>
2013-03-31 00:03:08 -0400

    Move queue into roomsec namespace

--- src/cpp/queue.h
+++ src/cpp/queue.h
@@ -5,59 +5,63 @@
 #include <queue>
 #include <boost/thread.hpp>
 
-template<typename T>
-class Queue {
-private:
-  std::queue<T> queue;
-  mutable boost::mutex mutex;
-  boost::condition_variable nonempty;
+namespace roomsec {
 
-public:
+  template<typename T>
+  class Queue {
+  private:
+    std::queue<T> queue;
+    mutable boost::mutex mutex;
+    boost::condition_variable nonempty;
 
-  T front() {
-    boost::unique_lock<boost::mutex> lock(this->mutex);
-    while (this->queue.empty()) {
-      this->nonempty.wait(lock);
+  public:
+
+    T front() {
+      boost::unique_lock<boost::mutex> lock(this->mutex);
+      while (this->queue.empty()) {
+	this->nonempty.wait(lock);
+      }
+      return this->queue.front();
     }
-    return this->queue.front();
-  }
 
-  T back() const {
-    boost::lock_guard<boost::mutex> guard(this->mutex);
-    return this->queue.back();
-  }
-
-  void push(T const& data) {
-    /* Atomic Section */
-    {
+    T back() const {
       boost::lock_guard<boost::mutex> guard(this->mutex);
-      this->queue.push(data);
+      return this->queue.back();
     }
 
-    this->nonempty.notify_one();
-    return;
-  }
+    void push(T const& data) {
+      /* Atomic Section */
+      {
+	boost::lock_guard<boost::mutex> guard(this->mutex);
+	this->queue.push(data);
+      }
 
-  void pop() {
-    boost::lock_guard<boost::mutex> guard(this->mutex);
-    this->queue.pop();
-    return;
-  }
-
-  T front_pop() {
-    boost::unique_lock<boost::mutex> lock(this->mutex);
-    while (this->queue.empty()) {
-      this->nonempty.wait(lock);
+      this->nonempty.notify_one();
+      return;
     }
-    T returnValue = this->queue.front();
-    this->queue.pop();
-    return returnValue;
-  }
 
-  bool empty() const {
-    boost::lock_guard<boost::mutex> guard(this->mutex);
-    return this->queue.empty();
-  }
-};
+    void pop() {
+      boost::lock_guard<boost::mutex> guard(this->mutex);
+      this->queue.pop();
+      return;
+    }
+
+    T front_pop() {
+      boost::unique_lock<boost::mutex> lock(this->mutex);
+      while (this->queue.empty()) {
+	this->nonempty.wait(lock);
+      }
+      T returnValue = this->queue.front();
+      this->queue.pop();
+      return returnValue;
+    }
+
+    bool empty() const {
+      boost::lock_guard<boost::mutex> guard(this->mutex);
+      return this->queue.empty();
+    }
+  };
+
+}
 
 #endif /* _ROOMSEC_QUEUE_H_ */
