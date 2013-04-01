2c1b827b62b240ddccd0fb2add3b62d39730ee4f
Robert Young <rwy0717@gmail.com>
2013-03-31 21:34:56 -0400

    Add additional debug statements to doorstatecontroller

--- src/cpp/doorstatecontroller.cpp
+++ src/cpp/doorstatecontroller.cpp
@@ -1,25 +1,30 @@
 #include "config.h"
 
 #include <boost/shared_ptr.hpp>
-#include <boost/bind.hpp>
+//#include <boost/bind.hpp>
 #include <boost/signal.hpp>
 #include <boost/thread.hpp>
+#include <log4cxx/logger.h>
 #include "actor.h"
 #include "doorstatesensor.h"
-
 #include "doorstatecontroller.h"
 
 namespace roomsec {
 
+  log4cxx::LoggerPtr DoorStateController::logger =
+    log4cxx::Logger::getLogger("roomsec.doorstatecontroller");
+
   DoorStateController::DoorStateController(boost::shared_ptr<DoorStateSensor> sensor)
     : sensor(sensor), stop(false)
   {
-
+    LOG4CXX_TRACE(logger, "DoorStateController Constructed");
   }
 
   void
   DoorStateController::run()
   {
+    LOG4CXX_DEBUG(logger, "DoorStateController running");
+
     DoorStateSensor::State oldState = sensor->getDoorState();
     while(!this->stop) {
       DoorStateSensor::State nextState = sensor->getDoorState();
@@ -30,6 +35,8 @@ namespace roomsec {
 
       boost::this_thread::sleep_for(boost::chrono::milliseconds(50));
     }
+
+    LOG4CXX_DEBUG(logger, "DoorStateController stopping");
   }
 
 }
--- src/cpp/doorstatecontroller.h
+++ src/cpp/doorstatecontroller.h
@@ -3,8 +3,10 @@
 #define _ROOMSEC_DOORSTATECONTROLLER_H_
 
 #include <boost/shared_ptr.hpp>
-#include <boost/bind.hpp>
+//#include <boost/bind.hpp>
 #include <boost/signals2.hpp>
+#include <log4cxx/logger.h>
+
 #include "actor.h"
 #include "doorstatesensor.h"
 
@@ -17,6 +19,7 @@ namespace roomsec {
     boost::signals2::signal<void (DoorStateSensor::State)> sigDoorStateChange;
     
   private:
+    static log4cxx::LoggerPtr logger;
     boost::shared_ptr<DoorStateSensor> sensor;
     bool stop;
 
