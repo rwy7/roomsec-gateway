fb64b52a09d6a26a3a526e8186c99452b0a0c7ce
Robert Young <rwy0717@gmail.com>
2013-04-20 02:57:46 -0400

    Add logging to thrift authority adapter

--- src/cpp/thriftauthorityadapter.cpp
+++ src/cpp/thriftauthorityadapter.cpp
@@ -6,6 +6,7 @@
 #include <transport/TSocket.h>
 #include <transport/TTransportUtils.h>
 
+#include <log4cxx/logger.h>
 #include "gen-cpp/Authority.h"
 #include "thriftauthorityadapter.h"
 
@@ -14,33 +15,45 @@ namespace roomsec {
   namespace transport = ::apache::thrift::transport;
   namespace protocol = ::apache::thrift::protocol;
 
+  static log4cxx::LoggerPtr logger2 = 
+    log4cxx::Logger::getLogger("roomsec.authorityadapter");
+
   ThriftAuthorityAdapter::ThriftAuthorityAdapter(const std::string& authorityServer, 
-						 int authorityPort) : 
-    socket(new transport::TSocket(authorityServer, authorityPort)),
-    transport(new transport::TBufferedTransport(socket)),
-    protocol(new protocol::TBinaryProtocol(transport)),
-    client(protocol) {
-    std::cout << "ThriftAuthorityAdapter initialized" << std::endl;
+						 int authorityPort)
+    : socket(new transport::TSocket(authorityServer, authorityPort)),
+      transport(new transport::TBufferedTransport(socket)),
+      protocol(new protocol::TBinaryProtocol(transport)),
+      client(protocol)
+  {
+    LOG4CXX_DEBUG(logger2, "ThriftAuthorityAdapter initialized");
   }
 
   ThriftAuthorityAdapter::~ThriftAuthorityAdapter() {
-    std::cout << "ThriftAuthorityAdapter destroyed" << std::endl;
+    LOG4CXX_DEBUG(logger2, "ThriftAuthorityAdapter destroyed");
   }
 
   void
   ThriftAuthorityAdapter::checkRequirementsImpl(std::vector<iface::CredentialSpec>& requirements,
 						const std::string& resource) {
+    LOG4CXX_DEBUG(logger2, "check requirements");
     this->transport->open();
+    LOG4CXX_DEBUG(logger2, "transport open");
     this->client.checkRequirements(requirements, resource);
+    LOG4CXX_DEBUG(logger2, "got results");
     this->transport->close();
+    LOG4CXX_DEBUG(logger2, "transport closed");
     return;
   }
 
   iface::AuthorizationReply::type
   ThriftAuthorityAdapter::authorizeImpl(const iface::AuthorizationRequest& request) {
+    LOG4CXX_DEBUG(logger2, "authorizing");
     this->transport->open();
+    LOG4CXX_DEBUG(logger2, "transport open");
     iface::AuthorizationReply::type reply = this->client.authorize(request);
+    LOG4CXX_DEBUG(logger2, "got reply");
     this->transport->close();
+    LOG4CXX_DEBUG(logger2, "transport closed");
     return reply;
   }
 }
